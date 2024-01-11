2dfd3a89f5a47f02acf8b932b88ae693c45a3284
BuKwon Kim <dnu05043@naver.com>
2024-01-11 17:34:26 +0900

    print +page.svelte list

--- src/main/java/com/ll/medium/global/rsData/RsData/RsData.java
+++ src/main/java/com/ll/medium/global/rsData/RsData/RsData.java
@@ -1,8 +1,10 @@
 package com.ll.medium.global.rsData.RsData;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
 
 import static lombok.AccessLevel.PRIVATE;
 
@@ -10,9 +12,13 @@ import static lombok.AccessLevel.PRIVATE;
 @NoArgsConstructor(access = PRIVATE)
 @Getter
 public class RsData<T> {
+    @NotNull
     String resultCode;
+    @NotNull
     int statusCode;
+    @NotNull
     String msg;
+    @NotNull
     T data;
 
     public static <T> RsData<T> of(String resultCode, String msg) {
@@ -25,10 +31,12 @@ public class RsData<T> {
         return new RsData<>(resultCode, statusCode, msg, data);
     }
 
+    @NonNull
     public boolean isSuccess() {
         return getStatusCode() >= 200 && getStatusCode() < 400;
     }
 
+    @NonNull
     public boolean isFail() {
         return !isSuccess();
     }
